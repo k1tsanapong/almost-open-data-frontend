e5df30c492cfc8a108338d204a060c73a5ea9dd4
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2024-01-23 03:31:57 +0700

    Vuetify Setup Guide, I guess.

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout> -->
+
+<div>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-text-field label="Name" />
+
+      </v-container>
+    </v-main>
+  </v-app>
+</div>
+
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  //...
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(
+          vuetify({
+            autoImport: true,
+            styles: {
+              configFile: "assets/settings.scss",
+            },
+          })
+        );
+      });
+    },
+    //...
+
+    '@nuxtjs/google-fonts',
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
+  googleFonts : {
+    // Options
+    families: {
+      Poppins: [ 100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    download: false,
+    useStylesheet: true,
+  },
+});
