afad87f09b10bf76c084d7a2e263fd0286c46d68
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2024-01-24 01:25:46 +0700

    Add dependencies for @vuepic/vue-datepicker and vue3-resize-text

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="pa-0">
     <v-row class="fill-height mt-2" no-gutters align="center">
-      <v-col cols="1">
+      <v-col cols="2" md="1">
         <v-img
           class="d-flex justify-end"
           src="/mfu_logo.png"
@@ -9,17 +9,28 @@
         />
       </v-col>
 
-      <v-col cols="3">
-        <div class="text-h3">MFU Open Data</div>
+      <v-col cols="4" md="3">
+        <div class="text-md-h5 text-lg-h4 font-weight-bold">MFU Open Data</div>
       </v-col>
 
       <v-spacer />
 
-      <v-col cols="1"> TH | EN </v-col>
+      <v-col cols="2" md="1">
+        <v-row>
+          <v-icon> mdi-xml </v-icon>
 
-      <v-col cols="1"> Developer </v-col>
+          Developer
+        </v-row>
+      </v-col>
 
-      <V-col cols="1"> Staff </V-col>
+      <v-col class="hidden-lg-and-up" cols="1"> </v-col>
+
+      <V-col cols="2" md="1">
+        <v-row>
+          <v-icon> mdi-exit-to-app </v-icon>
+          Staff
+        </v-row>
+      </V-col>
     </v-row>
 
     <v-row
@@ -28,30 +39,30 @@
       align="center"
       justify="center"
     >
-      <v-col v-ripple cols="1">
+      <v-col v-ripple md="2">
         <v-card class="text-center py-3" flat color="transparent">
-          <v-icon size="48">mdi-home-outline</v-icon>
+          <v-icon size="40">mdi-home-outline</v-icon>
           <div>Home</div>
         </v-card>
       </v-col>
 
-      <v-col v-ripple cols="1">
+      <v-col v-ripple md="2">
         <v-card class="text-center py-3" flat color="transparent">
-          <v-icon size="48">mdi-folder-open-outline</v-icon>
+          <v-icon size="40">mdi-folder-open-outline</v-icon>
           <div>Datasets</div>
         </v-card>
       </v-col>
 
-      <v-col v-ripple cols="1">
+      <v-col v-ripple md="2">
         <v-card class="text-center py-3" flat color="transparent">
-          <v-icon size="48"> mdi-folder-cog-outline</v-icon>
+          <v-icon size="40"> mdi-folder-cog-outline</v-icon>
           <div>Data Inventory</div>
         </v-card>
       </v-col>
 
-      <v-col v-ripple cols="1">
+      <v-col v-ripple md="2">
         <v-card class="text-center py-3" flat color="transparent">
-          <v-icon size="48">mdi-folder-file-outline</v-icon>
+          <v-icon size="40">mdi-folder-file-outline</v-icon>
           <div>Data Request</div>
         </v-card>
       </v-col>
@@ -60,17 +71,13 @@
     <v-row class="ma-0">
       <v-img src="/mfu_bg.png" cover max-height="30rem">
         <v-row no-gutters align="center" justify="center" class="fill-height">
-          <v-col cols="12" md="6" lg="5" sm="6">
+          <v-col cols="8" md="6">
             <v-text-field
               rounded="lg"
               bg-color="white"
               label="Search Datasets"
               variant="solo"
             ></v-text-field>
-
-            <div class="mt-5 d-flex justify-center"></div>
-
-            <div class="mt-5 d-flex justify-center"></div>
           </v-col>
         </v-row>
       </v-img>
@@ -83,8 +90,8 @@
           </v-divider>
         </v-col>
 
-        <v-col cols="1">
-          <div class="text-center text-h5">Group</div>
+        <v-col cols="2" md="1">
+          <div class="text-center text-h5 font-weight-bold">Group</div>
         </v-col>
 
         <v-col>
@@ -95,128 +102,105 @@
     </v-row>
 
     <v-row>
+      <v-col cols="1" md="2" lg="3"> </v-col>
 
-  
+      <v-col>
+        <v-row>
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >School</span
+              >
+            </v-card>
+          </v-col>
 
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >Centre</span
+              >
+            </v-card>
+          </v-col>
 
-      <v-col class="corner-row" cols="2" />
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >Institution</span
+              >
+            </v-card>
+          </v-col>
 
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">School</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >Academic Office</span
+              >
+            </v-card>
+          </v-col>
 
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-white text-center fill-height d-flex align-center justify-center"
+                >Central Administrative Office</span
+              >
+            </v-card>
+          </v-col>
 
-        <v-col cols="1" />
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >University Council Office</span
+              >
+            </v-card>
+          </v-col>
 
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >Internal Audit
+              </span>
+            </v-card>
+          </v-col>
 
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">Centre</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-white text-center fill-height d-flex align-center justify-center"
+                >Property and Asset Management Office</span
+              >
+            </v-card>
+          </v-col>
 
-        <v-col cols="1" />
+          <v-col cols="6" md="4" class="d-flex justify-center">
+            <v-card class="custom-card" width="180px" height="80px">
+              <span
+                class="text-h6 text-white text-center fill-height d-flex align-center justify-center"
+                >Special Unit</span
+              >
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
 
-
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">Institution</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col class="corner-row" cols="2" />
-
-        <v-col class="corner-row" cols="2" />
-
-<v-col cols="2">
-  <v-card class="custom-card" color="red" dark>
-    <v-card-text class="text-center">
-      <div class="text-h6">Academic Office</div>
-    </v-card-text>
-  </v-card>
-</v-col>
-
-
-<v-col cols="1" />
-
-
-<v-col cols="2">
-  <v-card class="custom-card" color="red" dark>
-    <v-card-text class="text-center">
-      <div class="text-h6">Central Administrative Office</div>
-    </v-card-text>
-  </v-card>
-</v-col>
-
-<v-col cols="1" />
-
-
-<v-col cols="2">
-  <v-card class="custom-card" color="red" dark>
-    <v-card-text class="text-center">
-      <div class="text-h6">University Council Office</div>
-    </v-card-text>
-  </v-card>
-</v-col>
-
-<v-col class="corner-row" cols="2" />
-
- <v-col class="corner-row" cols="2" />
-
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">Internal Audit
-
-</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-
-        <v-col cols="1" />
-
-
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">Property and Asset Management Office</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col cols="1" />
-
-
-        <v-col cols="2">
-          <v-card class="custom-card" color="red" dark>
-            <v-card-text class="text-center">
-              <div class="text-h6">Special Unit</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col class="corner-row" cols="2" />
-        
-
-
-  
-
-
-
+      <v-col cols="1" md="2" lg="3"> </v-col>
     </v-row>
+
+    <!-- <div >Hello Vue</div> -->
   </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const value = ref(new Date());
+</script>
 
 <style scoped>
 .custom-card {
