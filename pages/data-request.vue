64ff80a27f4da6622201af0248f1ebfc260f68ae
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2024-01-29 09:52:39 +0700

    data-request

--- pages/data-request.vue
+++ pages/data-request.vue
@@ -2,12 +2,15 @@
   <div>
     <v-img src="/mfu_bg.png" cover>
       <v-row class="my-16">
-        <v-col cols="2"/>
+        <v-col cols="2" />
 
         <v-col cols="12" sm="8">
           <v-row>
             <v-col>
-              <div class="font-weight-medium text-h6" :class="{'text-center': $vuetify.display.xs}">
+              <div
+                class="font-weight-medium text-h6"
+                :class="{ 'text-center': $vuetify.display.xs }"
+              >
                 Request for Dataset form
               </div>
             </v-col>
@@ -25,6 +28,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="firstName"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example : John"
@@ -42,6 +46,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="lastName"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example : Doe"
@@ -59,6 +64,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="email"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example : *@lamduan.mfu.ac.th"
@@ -76,6 +82,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="datasetName"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example: Information about students who did not complete their studies"
@@ -93,6 +100,7 @@
                     </v-row>
                     <v-row>
                       <v-textarea
+                        v-model="datasetDetails"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example: Data on students who did not complete their studies within the last 5 years, categorized by faculty and major"
@@ -110,6 +118,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="dataOwner"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example: Information about students who did not complete their studies"
@@ -127,6 +136,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="purposeOfUse"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example: For research purposes, for BI (Business Intelligence)"
@@ -144,6 +154,7 @@
                     </v-row>
                     <v-row>
                       <v-text-field
+                        v-model="datasetGroup"
                         rounded="lg"
                         bg-color="white"
                         placeholder="Example: For research purposes, for BI (Business Intelligence)"
@@ -163,9 +174,8 @@
 
                   <v-col class="d-flex justify-center">
                     <v-btn
-                    class="mr-5"
-                    prepend-icon="mdi-close"
-
+                      class="mr-5"
+                      prepend-icon="mdi-close"
                       rounded="lg"
                       color="orange"
                       width="200"
@@ -175,13 +185,15 @@
                     </v-btn>
 
                     <v-btn
-                    prepend-icon="mdi-send"
+                      prepend-icon="mdi-send"
                       rounded="lg"
                       color="blue"
                       width="200"
                       height="50"
                     >
-                      <div class="text-h6 font-weight-bold">Submit</div>
+                      <div @click="submitForm" class="text-h6 font-weight-bold">
+                        Submit
+                      </div>
                     </v-btn>
                   </v-col>
 
@@ -191,16 +203,76 @@
             </v-col>
           </v-row>
         </v-col>
-        <v-col cols="2"/>
+        <v-col cols="2" />
       </v-row>
     </v-img>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+const { $swal } = useNuxtApp();
+import { reloadNuxtApp } from "nuxt/app";
 
-const value = ref(new Date());
+const firstName = ref("");
+const lastName = ref("");
+const email = ref("");
+const datasetName = ref("");
+const datasetDetails = ref("");
+const dataOwner = ref("");
+const purposeOfUse = ref("");
+const datasetGroup = ref("");
+
+// const { post, response } = useMyFetch();
+
+const submitForm = async () => {
+  const formData = {
+    requester_first_name: firstName.value,
+    requester_last_name: lastName.value,
+    // email: email.value,
+    dataset_name: datasetName.value,
+    dataset_details: datasetDetails.value,
+    data_owner: dataOwner.value,
+    purpose_of_use: purposeOfUse.value,
+    dataset_group: datasetGroup.value,
+  };
+
+  console.log(formData);
+
+  try {
+    const { data } = await useMyFetch("/api/request-datasets", {
+      method: "post",
+      body: {
+        data: formData,
+      },
+    });
+
+    $swal.fire({
+      title: "Submitted",
+      text: "Form submitted successfully",
+      icon: "success",
+      didClose: () =>
+        reloadNuxtApp({
+          path: "/data-request",
+          ttl: 1000, // default 10000
+        }),
+    });
+
+    console.log("Form submitted successfully:", data);
+    console.log(data);
+  } catch (error) {
+    $swal.fire({
+      title: "Error!",
+      icon: "error",
+      didClose: () =>
+        reloadNuxtApp({
+          path: "/data-request",
+          ttl: 1000, // default 10000
+        }),
+    });
+    console.error("Error during form submission:", error);
+  }
+};
 </script>
 
 <style scoped>
